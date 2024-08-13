69b1e988a805c9b7fb9c85c552cf81f6b810f2b9
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-08-13 10:40:25 +0200

    Completed the tasks!

--- csharp-fundamentals-lists.Main/Core.cs
+++ csharp-fundamentals-lists.Main/Core.cs
@@ -32,16 +32,19 @@ namespace csharp_fundamentals_lists.Main
 
             //write code here
 
+            _iceCreams.Add("Phish Food");
+            _iceCreams.Add("Peanut Butter Cup");
+
             return _iceCreams;        
         }
 
         public int Question2()
         {
-            
+
             //TODO:  find the lists method that returns the number of ice creams in the list and return this.
 
             // remove exception and write code here
-            throw new NotImplementedException();
+            return _iceCreams.Count;
         }
         public List<string> Question3()
         {
@@ -49,12 +52,12 @@ namespace csharp_fundamentals_lists.Main
             // The code below concatenates this.MoreIceCream to the _iceCreams list into a new results list.
             //TODO: you can 'chain' methods on the _iceCream list, so add another Concat to include EvenMoreIceCream  (this is defined below) to the result list . e.g.   _iceCreams.Concat(this.MoreIceCream).Concat(other list to concat).ToList()
 
-            List<string> results = _iceCreams.Concat(this.MoreIceCream).ToList();
+            List<string> results = _iceCreams.Concat(this.MoreIceCream).ToList().Concat(this.EvenMoreIceCream).ToList();
             
             return results;
 
             // remove exception and write code here
-
+            //Wrote the code above instead because I cannot write more after return.
         }
         public List<string> Question4()
         {
@@ -67,6 +70,7 @@ namespace csharp_fundamentals_lists.Main
 
             List<string> results = _iceCreams;
             // remove exception and write code here
+            results = _iceCreams.Concat(this.MoreIceCream).ToList().Concat(this.EvenMoreIceCream).ToList().Distinct().ToList();
             return results;
         }
 

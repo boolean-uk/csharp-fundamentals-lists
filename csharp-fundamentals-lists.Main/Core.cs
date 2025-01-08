9ce00b2630b2aa4bca5ec95c69cbc7cf2157074c
magnus195 <22998711+magnus195@users.noreply.github.com>
2025-01-08 10:25:45 +0100

    Removed unused imports

--- csharp-fundamentals-lists.Main/Core.cs
+++ csharp-fundamentals-lists.Main/Core.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp_fundamentals_lists.Main
+﻿namespace csharp_fundamentals_lists.Main
 {
     public class Core
     {
